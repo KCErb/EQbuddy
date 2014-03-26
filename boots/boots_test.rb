67273dd4312d35880456d10e4155d2a3c68173e0
KC Erb <iamkcerb@gmail.com>
2014-03-26 15:11:28 -0600

    added tests for medium width - still need to debug .333 + .333 + .333 != 1 issue

--- boots/boots_test.rb
+++ boots/boots_test.rb
@@ -1,14 +1,18 @@
 require_relative 'col'
 require_relative 'row'
-
-Shoes.app width: 1200 do
+Shoes.app width: 1000 do
   row do
-    col lg: 6 do
+    col lg: 6, md: 4 do
       background purple
-      para "Content"
+      para "content"
     end
-    col lg: 6 do
+    col lg: 6, md: 4 do
       background red
+      para "content"
+    end
+  
+    col lg: 6, md: 4 do
+      background blue
       para "content"
     end
   end
