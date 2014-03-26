e633ff1342401f709a644344bd817272c23908e7
KC Erb <iamkcerb@gmail.com>
2014-03-25 20:51:46 -0600

    col and row working now, still need improvements though

--- boots/boots_test.rb
+++ boots/boots_test.rb
@@ -1,7 +1,17 @@
 require_relative 'col'
+require_relative 'row'
 
-Shoes.app do
-  col do
-    para "My width is #{width}"
+Shoes.app width: 1200 do
+  row do
+    col lg: 6 do
+      background purple
+      para "Content"
+    end
+    col lg: 6 do
+      background red
+      para "content"
+    end
   end
 end
+
+
--- boots/row.rb
+++ boots/row.rb
@@ -2,5 +2,8 @@ class Row < Shoes::Widget
   def initialize(*opts, &blk)
     @opts = opts
     @blk = blk
+    flow do
+      blk.call
+    end
   end
 end
